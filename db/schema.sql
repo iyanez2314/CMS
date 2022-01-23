7eba0f79f322afffa1659b7a33a2ab1cf845a23c
Isaac yanez <isaac231467@gmail.com>
2022-01-23 00:34:59 -0600

    added schema

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department(
+id INT PRIMARY KEY,
+name VARCHAR(30)
+);
+
+CREATE TABLE role (
+id INT PRIMARY KEY,
+title VARCHAR(30),
+salary DECIMAL(6,2),
+department_id INT
+);
+
+CREATE TABLE employee(
+id INT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT NOT NULL
+)
